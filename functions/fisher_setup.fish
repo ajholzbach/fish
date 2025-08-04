672978f39ee1e55b5df9f26475b7d0703a8629f0
Alex Holzbach <ajholzbach>
2025-08-04 15:24:36 +0800

    Removed fisher_setup

--- functions/fisher_setup.fish
+++
@@ -1,3 +0,0 @@
-function fisher_setup --description 'Bootstrap Fisher and plugins'
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
-end
