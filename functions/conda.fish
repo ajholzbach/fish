7985806f37b2c7379a08c0344683b3dabc034e05
Alex Holzbach <ajholzbach>
2025-08-05 13:36:06 +0800

    Added fzf.fish and updated conda.fish

--- functions/conda.fish
+++ functions/conda.fish
@@ -4,12 +4,8 @@ function conda --wraps conda
     if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
         eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" | source
         conda $argv
-    else
-        if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-            . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        else
-            set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
-        end
+    else if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
     end
     # <<< conda initialize <<<
 end
