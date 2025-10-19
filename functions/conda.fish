d0cc6bbcd0c14170878eb34f85cea5588ea5706e
Alex Holzbach <ajholzbach>
2025-10-19 19:15:33 -0400

    Moved git aliases to fish-git

--- functions/conda.fish
+++ functions/conda.fish
@@ -1,11 +1,11 @@
 function conda --wraps conda
     functions --erase conda
     # >>> conda initialize >>>
-    if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-        eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" | source
+    if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+        eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" | source
         conda $argv
-    else if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
     end
     # <<< conda initialize <<<
 end
