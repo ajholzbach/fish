6be6194b990312100da66dbc3e4280340bb013f2
Alex Holzbach <ajholzbach>
2025-10-20 00:09:46 -0400

    Updated conda.fish

--- functions/conda.fish
+++ functions/conda.fish
@@ -1,11 +1,40 @@
 function conda --wraps conda
     functions --erase conda
-    # >>> conda initialize >>>
-    if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
-        eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" | source
-        conda $argv
-    else if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+
+    set -l conda_exe
+    if set -q CONDA_EXE; and test -x "$CONDA_EXE"
+        set conda_exe "$CONDA_EXE"
+    else if command -sq conda
+        set conda_exe (command -s conda)
     end
-    # <<< conda initialize <<<
+
+    if test -z "$conda_exe"
+        set -l prefixes \
+            $HOME/miniforge3 $HOME/miniconda3 $HOME/anaconda3 $HOME/mambaforge \
+            /opt/miniforge3 /opt/miniconda3 /opt/anaconda3 /opt/conda
+
+        if command -sq brew
+            set -l brew_prefix (brew --prefix ^/dev/null)
+            set -a prefixes \
+                $brew_prefix/Caskroom/miniforge/base \
+                $brew_prefix/Caskroom/miniconda/base \
+                $brew_prefix/Caskroom/mambaforge/base \
+                $brew_prefix/Caskroom/anaconda/base
+        end
+
+        for p in $prefixes
+            if test -x "$p/bin/conda"
+                set conda_exe "$p/bin/conda"
+                break
+            end
+        end
+    end
+
+    if test -z "$conda_exe"
+        echo "conda: not found (checked \$PATH, CONDA_EXE, and common locations)" >&2
+        return 127
+    end
+
+    $conda_exe "shell.fish" "hook" | source
+    conda $argv
 end
