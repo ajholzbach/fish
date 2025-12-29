343da3b70cb1a3ce57f7d3aae7db7a259e3c94de
Alex Holzbach <ajholzbach>
2025-12-29 11:32:58 -0500

    Fixed cat coloring

--- functions/cat.fish
+++ functions/cat.fish
@@ -1,7 +1,7 @@
 function cat --wraps cat
     functions --erase cat
     if type -q bat
-        alias cat='bat --paging=never --style=plain --color=always'
+        alias cat='bat --paging=never --style=plain --color=auto'
     end
     cat $argv
 end
